05c41441eaacbc5ea9f53274c93485ba312d3dcf
Zefeng Fei <z5089786@ad.unsw.edu.au>
2023-08-27 10:38:45 +1000

    add a function that Convert temperatures between Fahrenheit and Celsius.

---
+++ temperature_convert/target/debug/deps/temperature_convert-8486786575a097e1.d
@@ -0,0 +1,5 @@
+/Users/oldcow/github/learnRust/temperature_convert/target/debug/deps/temperature_convert-8486786575a097e1: src/main.rs
+
+/Users/oldcow/github/learnRust/temperature_convert/target/debug/deps/temperature_convert-8486786575a097e1.d: src/main.rs
+
+src/main.rs:
---
+++ temperature_convert/target/debug/temperature_convert.d
@@ -0,0 +1,1 @@
+/Users/oldcow/github/learnRust/temperature_convert/target/debug/temperature_convert: /Users/oldcow/github/learnRust/temperature_convert/src/main.rs
