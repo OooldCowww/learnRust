8ca02f067682f84b2b0bf05797f6b920bba57366
Zefeng Fei <z5089786@ad.unsw.edu.au>
2023-08-27 10:23:23 +1000

    Generate the nth Fibonacci number

---
+++ fibonacci/target/debug/deps/fibonacci-8e948c83c6757c1e.d
@@ -0,0 +1,5 @@
+/Users/oldcow/github/learnRust/fibonacci/target/debug/deps/fibonacci-8e948c83c6757c1e.rmeta: src/main.rs
+
+/Users/oldcow/github/learnRust/fibonacci/target/debug/deps/fibonacci-8e948c83c6757c1e.d: src/main.rs
+
+src/main.rs:
---
+++ fibonacci/target/debug/deps/fibonacci-c945bfcb1e93ab1c.d
@@ -0,0 +1,5 @@
+/Users/oldcow/github/learnRust/fibonacci/target/debug/deps/fibonacci-c945bfcb1e93ab1c.rmeta: src/main.rs
+
+/Users/oldcow/github/learnRust/fibonacci/target/debug/deps/fibonacci-c945bfcb1e93ab1c.d: src/main.rs
+
+src/main.rs:
